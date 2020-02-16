edae0cf863911534f3f05c97a7aba48edee5c906
Anuradha Weeraman <aweeraman@gmail.com>
2020-02-16 16:34:22 -0500

    Cleanup

--- boot.sh
+++ boot.sh
@@ -65,6 +65,7 @@ if test "${copy_modules_to_rootfs}x" = "yx"; then
       KERNEL_PATH=${srcdir}/${kernel} make clean
       KERNEL_PATH=${srcdir}/${kernel} PROCS=${procs} make all
       sudo KERNEL_PATH=${srcdir}/${kernel} INSTALL_MOD_PATH=${rootfs} make install
+      KERNEL_PATH=${srcdir}/${kernel} make clean
     )
   fi
 
